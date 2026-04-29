4878cedd38de597e577a6262ade927ae5059f4ba
Sidne <contato.sidnecabo@gmail.com>
2026-04-29 15:51:15 -0300

    Add initial data inserts for usuarios and produtos tables

---
+++ inserts.sql
@@ -0,0 +1,47 @@
+INSERT INTO usuarios(nome, email, senha, cpf, endereco) 
+VALUES('Cleverson' , 
+       'cleverson123@gmail.com', 
+       'cleverson123#2', 
+       '12345678910', 
+       'Rua 1 Queda Dágua'	
+);
+
+INSERT INTO usuarios(nome, email, senha, cpf, endereco) 
+VALUES('Jhonsom' , 
+       'jhonsomemail@gmail.com', 
+       'dunga123@wwe', 
+       '11111119870', 
+       'Rua 1 Queda de Chuva'	
+);
+
+INSERT INTO usuarios(nome, email, senha, cpf, endereco) 
+VALUES('Roberto Leif' , 
+       'robertoleif@gmail.com', 
+       'clodoaldo77123@wwe', 
+       '77812619870', 
+       'Rua 10 Primeiro de Novembro'	
+);
+
+INSERT INTO usuarios(nome, email, senha, cpf, endereco) 
+VALUES('Luiz Clint' , 
+       'luizclint@gmail.com', 
+       'voltasemidateste', 
+       '25712610007', 
+       'Rua 332 Volta sem ida'	
+);
+
+
+INSERT INTO produtos(nome, preco)
+VALUES ('Pizza Pequena',
+		 49.99
+);
+
+INSERT INTO produtos(nome, preco)
+VALUES ('Pizza Média',
+		 60.90
+);
+
+INSERT INTO produtos(nome, preco)
+VALUES ('Pizza Grande',
+		 80.99
+);
